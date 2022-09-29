6d4710b3391578bc06f226262c5049eef86a8b6f
Tom <81436713+CurtainsAreBetter@users.noreply.github.com>
2022-09-29 13:07:51 -0400

    Reorganize write-number-in-expanded-form.js

--- write-number-in-expanded-form.js
+++ write-number-in-expanded-form.js
@@ -1,8 +1,9 @@
 //https://www.codewars.com/kata/5842df8ccbd22792a4000245/
 function expandedForm(num) {
-  let expanded = num.toString().split('');
-  expanded = expanded.map((v, i, arr) => {
-    return Number(v) * 10**(arr.length - (i+1));
-  }).filter(v => v).map(v => v.toString()).join(' + ');
+  const expanded = num.toString()
+        .split('')
+        .map((v, i, arr) => Number(v) * 10**(arr.length - (i+1)))
+        .filter(v => v)
+        .join(' + ');
   return expanded;
 }
